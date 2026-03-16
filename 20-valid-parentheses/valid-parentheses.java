e0844012f4bba3915269c83a521d63352f63450a
Praveen Kumar A <apraveenkumar0065@gmail.com>
2026-03-16 16:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (91.41%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 == 1)
+            return false;
+        return isValid(0, ' ', s.toCharArray()) >= 0;
+    }
+
+    private int isValid(int i,char last, char[] s){
+        if(i == s.length){
+            return i;
+        }
+        if(i > s.length){
+            return -1;
+        }
+
+        if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+            int next = isValid(i + 1, s[i], s);
+            return next < 0 ? -1 : isValid(next + 1, last, s);
+        }
+
+        if(s[i] == ')' && last == '(' || s[i] == ']' && last == '[' ||s[i] == '}' && last == '{'){
+            return i;
+        }
+        return -1;
+    }
+
+}
