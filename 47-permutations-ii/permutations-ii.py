9c0b3666d33a68f1d4ed101b5573f2c371690a63
Praveen Kumar A <apraveenkumar0065@gmail.com>
2026-08-28 11:17:34 +0530

    Time: 7 ms (39.68%) | Memory: 20 MB (23.46%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -5,23 +5,15 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         results = []
         def backtrack(comb, counter):
-            # If the current combination is done
             if len(comb) == len(nums):
                 results.append(list(comb))
-                return
-            
+                return            
             for num in counter:
                 if counter[num] > 0:
-                    # Choose the number
                     comb.append(num)
                     counter[num] -= 1
-                    
-                    # Continue down the recursion tree
                     backtrack(comb, counter)
-                    
-                    # Revert the choice (backtrack)
                     counter[num] += 1
                     comb.pop()
-                    
         backtrack([], Counter(nums))
         return results
